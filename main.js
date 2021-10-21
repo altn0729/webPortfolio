22e5c0887bc519b17977853137d3ee90e04f45d6
박인수 <altn0729@naver.com>
2021-10-21 20:04:13 +0900

    Update JavaScript selected navbar item
    navbar item 클릭 시 border 효과

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 'use strict';
 
-// navbar scroll시 배경 색깔 변경 및 에니메이션
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 const navbarMenu = document.querySelector('.navbar__menu');
 console.log(navbarMenu);
 
-const about = document.querySelector('#about');
-const aboutScrollY = about.offsetTop - 200;
-console.log(`aboutScrollY: ${aboutScrollY}`);
-
+// scroll시 navbar 상단 고정 및 배경 색깔 변경, 에니메이션
 document.addEventListener('scroll', () => {
     // console.log(`window.scrollY: ${window.scrollY}`);
     // console.log(`navbarHeight: ${navbarHeight}`);
@@ -22,17 +18,40 @@ document.addEventListener('scroll', () => {
     }
 });
 
+// navbar에서 선택된 아이템 테두리 효과 및 선택된 아이템(Section) 위치로 이동
 navbarMenu.addEventListener('click', (event) => {
+    const activeItem = document.querySelector('.navbar__menu > .active');
+    const activeValue = activeItem.dataset.value;
+
+    // console.log(`Active 상태인 li Tag: ${activeItem.innerHTML}`);
+    // console.log(`Active 상태인 아이템의 Tag의 value 값: ${activeValue}`);
+
     const target = event.target
     const value = target.dataset.value;
 
-    // console.log(target);
-    // console.log(value);
+    // console.log(`선택된 아이템 li Tag: ${target.innerHTML}`);
+    // console.log(`선택된 아이템의 Tag value 값: ${value}`);
 
     if (value == null) {
         return;
     }
 
-    const scrollTo = document.querySelector(value);
-    scrollTo.scrollIntoView({behavior:'smooth', block:'center'});
+    const selectedItem = document.querySelector(value);
+    selectedItem.scrollIntoView({behavior:'smooth', block:'center'});
+
+    if (activeValue === value) {
+        target.classList.remove('active');
+        activeItem.classList.add('acitve');
+    } else {
+        activeItem.classList.remove('active');
+        target.classList.add('active');
+    }
+});
+
+const toggleBtn = document.querySelector('.navbar__toggle-btn');
+console.log(toggleBtn);
+
+toggleBtn.addEventListener('click', () => {
+    console.log('click!');
+    navbarMenu.style.display = 'block';
 });
